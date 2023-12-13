f6954f1adcd04aa3a730992c8c9cbc66283182df
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2023-12-13 09:51:54 +0100

    Implement check if fruit has no effect

--- src/main/java/eu/wxrlds/beetifulgarden/item/BeetifulItem.java
+++ src/main/java/eu/wxrlds/beetifulgarden/item/BeetifulItem.java
@@ -29,19 +29,20 @@ public class BeetifulItem {
     }
 
     public static EffectInstance[] ParseConfigEffects(String effectString) {
-        String[] effectStrings = effectString.split("\\|");
-        EffectInstance[] effectInstances = new EffectInstance[effectStrings.length];
-        for (int i = 0; i < effectStrings.length; i++) {
-            String[] parts = effectStrings[i].split(":");
-            String modID = parts[0];
-            String effectID = parts[1];
-            int duration = Integer.parseInt(parts[2]);
-            int amplifier = Integer.parseInt(parts[3]);
-
-            // Create and store the EffectInstance
-            effectInstances[i] = new EffectInstance(ForgeRegistries.POTIONS.getValue(new ResourceLocation(modID, effectID)), duration, amplifier);
+        if (!effectString.isEmpty()) {
+            String[] effectStrings = effectString.split("\\|");
+            EffectInstance[] effectInstances = new EffectInstance[effectStrings.length];
+            for (int i = 0; i < effectStrings.length; i++) {
+                String[] parts = effectStrings[i].split(":");
+                String modID = parts[0];
+                String effectID = parts[1];
+                int duration = Integer.parseInt(parts[2]);
+                int amplifier = Integer.parseInt(parts[3]);
+                effectInstances[i] = new EffectInstance(ForgeRegistries.POTIONS.getValue(new ResourceLocation(modID, effectID)), duration, amplifier);
+            }
+            return effectInstances;
         }
-        return effectInstances;
+        return new EffectInstance[0];
     }
 
     public static final Food.Builder EMINENCE_PROPERTIES = (new Food.Builder())
