ac988e8e47ac81f902e39a5cff54fbe0a3265a9d
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2025-06-22 22:42:54 +0200

    Don't reinvent the wheel for tooltips (kinda)

--- src/main/java/eu/wxrlds/beetifulgarden/block/crop/VerdigrisCrop.java
+++ src/main/java/eu/wxrlds/beetifulgarden/block/crop/VerdigrisCrop.java
@@ -1,39 +1,30 @@
 package eu.wxrlds.beetifulgarden.block.crop;
 
-import eu.wxrlds.beetifulgarden.block.ModBlocks;
 import eu.wxrlds.beetifulgarden.config.BeetifulGardenCommonConfigs;
 import eu.wxrlds.beetifulgarden.item.ModItems;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.BeetrootBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.IPlantable;
-import net.minecraftforge.common.PlantType;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class VerdigrisCrop extends BeetrootBlock implements IPlantable {
-    Block plantableOn = ModBlocks.ParseConfigPlantableBlock(BeetifulGardenCommonConfigs.VERDIGRIS_PLANTABLE_ON.get());
+    private Block getPlantableOn() {
+        String configValue = BeetifulGardenCommonConfigs.VERDIGRIS_PLANTABLE_ON.get();
+        return ForgeRegistries.BLOCKS.getValue(new ResourceLocation(configValue));
+    }
 
     public VerdigrisCrop(Properties properties) {
         super(properties);
     }
 
-    // Remove the plant type so that it cannot be placed on farmland
-    @Override
-    public PlantType getPlantType(BlockGetter world, BlockPos pos) {
-        return null;
-    }
-
-    @Override
-    public boolean canSustainPlant(BlockState state, BlockGetter world, BlockPos pos, Direction facing, IPlantable plantable) {
-        return state.is(plantableOn);
-    }
-
     @Override
     protected boolean mayPlaceOn(BlockState state, BlockGetter worldIn, BlockPos pos) {
-        return state.is(plantableOn);
+        return state.is(getPlantableOn());
     }
 
     @Override
